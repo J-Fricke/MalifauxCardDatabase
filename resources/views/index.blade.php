234f326f670d6cdf95f7c88962f940342021044e
joshua <git@newsun.org>
2015-12-30 13:35:06 -0800

    feat: setup implicit controller and init a base layout for the site

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.baselayout')
+@section('title')
+    Malifaux Card Database
+@endsection
+@section('content')
+    <div class="row">
+        <h1>Malifaux Card Database</h1>
+    </div>
+@endsection
